e59abddb96ab634e882b8c3a13a6be74c776a2dc
Max C <smashmouthmax@gmail.com>
2022-04-27 07:48:13 -0700

    fixed img link

--- src/home.js
+++ src/home.js
@@ -6,7 +6,7 @@ function loadHome() {
     const homeTitle = createHTMLElement('div', null, ['home-title'], 'Welcome to Allo Eatery');
     const homeSubtitle = createHTMLElement('div', null, ['home-subtitle'], 'A Taste of Italy');
     const foodImage = createHTMLElement('img', null, ['home-image'], null);
-    foodImage.setAttribute('src', '/src/assets/photo.jpg');
+    foodImage.setAttribute('src', '../src/assets/photo.jpg');
     const restaurantBio = createHTMLElement('div', null, ['home-bio'], `Allo Eatery has been serving the community since 1969. 
     With a passion for quality and using only the finest ingredients around, 
     Allo Eatery will certainly leave you satisfied and wanting more.`);
